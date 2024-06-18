6cf610b0dfe5bfd5b945bc2658b60a63aa132335
Sergio Cusa <sergiocusa.91@gmail.com>
2024-06-18 16:50:00 -0300

    navigation

--- 4-en-linea/services/contacts.js
+++ 4-en-linea/services/contacts.js
@@ -1,25 +1,5 @@
 const BASE_URL = 'https://us-central1-api-nt2-ejemplo.cloudfunctions.net/app/api/'
 
-// const nombres = ['Carlos', 'Paula', 'Lionel', 'Elena', 'Mateo', `Enzo`]
-// const apellidos = ['Messi', 'Perez', 'Romero', 'Gomez', 'Di Maria', `Martinez`]
-
-// const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-
-// const generaNombre = () => `${nombres[random(0, nombres.length - 1)]}`
-// const generaApellido = () => `${apellidos[random(0, apellidos.length - 1)]}`
-
-
-// //Telefono seria: 55-555-555
-// const generarTelefono = () => `${random(10, 99)}-${random(100, 999)}-${random(100, 999)}`
-
-// const crearContacto = () => {
-//   return {
-//     fullName: `${generaApellido()}, ${generaNombre()}`,
-//     phone: generarTelefono(),
-//     age: random(18, 99)
-//   }
-// }
 
 
 const getContacts = () => {
