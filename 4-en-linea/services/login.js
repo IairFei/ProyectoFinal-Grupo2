4361f7942a3b8045c040b3bbd4e94e65e5ea1e88
martinchichi <martincicinelli@gmail.com>
2024-06-22 17:50:51 -0300

    se agrega hashPassword en login.js  para hashear contraseñas

--- 4-en-linea/services/login.js
+++ 4-en-linea/services/login.js
@@ -1,29 +1,38 @@
 import AsyncStorage from "./AsyncStorage";
+import bcrypt from "bcrypt";
 
-const login = (email, password) => {
-    return new Promise((resolve, reject) => {
-      //TODO: Implementar la llamada al API
-      const shouldLogin = email.toString().toLowerCase() === 'admin@admin.com' && password === 'admin'
-      if (shouldLogin) {
-        const authData = {
-          access_token: '123456789',
-          expires_in: 3600,
-          profile: {
-            email: 'admin@admin.com',
-            fullName: "Admin",
-            role: 'admin'
-          }
-        }
-        AsyncStorage.storeData('authData', authData);
-        return resolve(authData);
-      } else {
-        return reject('Usuario o contraseña incorrectos');
-      }
-    });
+
+const hashPassword = async (password) => {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
 }
 
-const register = (email, password, fullName) => {
+const login = (email, password) => {
   return new Promise((resolve, reject) => {
+    // TODO: Implementar la llamada al API
+    const shouldLogin = email.toString().toLowerCase() === 'admin@admin.com' && password === 'admin'
+    if (shouldLogin) {
+      const authData = {
+        access_token: '123456789',
+        expires_in: 3600,
+        profile: {
+          email: 'admin@admin.com',
+          fullName: "Admin",
+          role: 'admin'
+        }
+      }
+      AsyncStorage.storeData('authData', authData);
+      return resolve(authData);
+    } else {
+      return reject('Usuario o contraseña incorrectos');
+    }
+  });
+}
+
+const register = async (email, password, fullName) => {
+  try {
+    const hashedPassword = await hashPassword(password);
     const authData = {
       access_token: '123456789',
       expires_in: 3600,
@@ -31,11 +40,14 @@ const register = (email, password, fullName) => {
         email,
         fullName,
         role: 'user'
-      }
+      },
+      hashedPassword
     };
-    AsyncStorage.storeData('authData', authData);
-    return resolve(authData);
-  });
+    await AsyncStorage.storeData('authData', authData);
+    return Promise.resolve(authData);
+  } catch (error) {
+    return Promise.reject('Error registrando el usuario');
+  }
 };
 
 const logout = async () => {
